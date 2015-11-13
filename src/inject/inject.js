de0d11751d064bc56e1f54471f39805b06b739a9
Kyle Brumm <kjbrum@msn.com>
2015-11-13 17:57:02 -0600

    Remove the check/uncheck button for now

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,7 +10,8 @@
 $('.list-card-details').click( function() {
     setTimeout(function() {
         // If autohide checked
-        autohideCheckedItems();
+        toggleCheckedItems('hide');
+        addCheckedToggle('show');
 
         // If add check/uncheck all button
         addCheckUncheckButton();
@@ -26,7 +27,8 @@ $(document).ready(function() {
     if( $('.js-checklist-list').length ) {
         setTimeout(function() {
             // If autohide checked
-            autohideCheckedItems();
+            toggleCheckedItems('hide');
+            addCheckedToggle('show');
 
             // If add check/uncheck all button
             addCheckUncheckButton();
@@ -37,12 +39,16 @@ $(document).ready(function() {
 
 
 /*************************************************
- *  Event: Button is clicked
+ *  Event: A button is clicked
  *************************************************/
 $(document).on('click', '.js-tt-toggle-checked', function() {
     var action = $(this).data('action');
     toggleCheckedItems(action);
 });
+
+// $(document).on('click', '.js-tt-toggle-checkuncheck', function() {
+//     toggleCheckUncheckItems($(this));
+// });
 
 
 
@@ -55,32 +61,43 @@ function toggleCheckedItems(action) {
         this.click();
     });
 
-    if( $('.js-tt-toggle-checked').length ) {
-        var text = $('.js-tt-toggle-checked').html();
+    var $btn = $('.js-tt-toggle-checked');
+    if( $btn.length ) {
+        var text = $btn.html();
         if( action == 'show' ) {
-            $('.js-tt-toggle-checked').data('action', 'hide').html(text.replace('Show', 'Hide'));
+            $btn.data('action', 'hide').html(text.replace('Show', 'Hide'));
         } else {
-            $('.js-tt-toggle-checked').data('action', 'show').html(text.replace('Hide', 'Show'));
+            $btn.data('action', 'show').html(text.replace('Hide', 'Show'));
         }
     }
 };
+
+// Toggle checking/unchecking items
+// function toggleCheckUncheckItems(el) {
+//     var action = el.data('action');
+
+//     // Grab all the element and check/uncheck
+
+//     if( el.length ) {
+//         var text = el.text();
+//         if( action == 'check' ) {
+//             el.data('action', 'uncheck').text(text.replace('Check', 'Uncheck'));
+//         } else {
+//             el.data('action', 'check').text(text.replace('Uncheck', 'Check'));
+//         }
+//     }
+// };
 
 // Add a button for toggling checked items
 function addCheckedToggle(action) {
     $('.window-module.other-actions .u-clearfix').append('<a class="button-link js-tt-toggle-checked" title="Toggle the finished checklist items." data-action="'+action+'"><span class="icon-sm icon-subscribe"></span> '+toTitleCase(action)+' Checked</a>');
 }
 
-// Autohide all the checked list items
-function autohideCheckedItems() {
-    toggleCheckedItems('hide');
-    addCheckedToggle('show');
-}
-
 // Add check/uncheck all button
-function addCheckUncheckButton() {
-    $('.window-module.checklist-list .window-module-title-options').prepend('<a class="hide-on-edit quiet js-check-all-items" href="#">Check All</a>');
-    console.log('add check/uncheck all button');
-}
+// function addCheckUncheckButton() {
+//     $('.window-module.checklist-list .window-module-title-options').prepend('<a class="hide-on-edit quiet js-tt-toggle-checkuncheck" href="#" data-action="check">Check All</a>');
+//     console.log('add check/uncheck all button');
+// }
 
 // Convert a string to title case
 function toTitleCase(str) {
