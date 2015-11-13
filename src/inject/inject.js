6eb3e3d6452d0ab4af008fc01811f56e529f0530
Kyle Brumm <kjbrum@msn.com>
2015-11-12 18:58:45 -0600

    Add autohide function and display 'check all' link above checklist

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,11 +8,12 @@
  *  Event: Card is clicked
  *************************************************/
 $('.list-card-details').click( function() {
-    // If autohide checked
     setTimeout(function() {
-        // Autohide items
-        toggleCheckedItems('hide');
-        addCheckedToggle('show');
+        // If autohide checked
+        autohideCheckedItems();
+
+        // If add check/uncheck all button
+        addCheckUncheckButton();
     }, 1000);
 });
 
@@ -22,12 +23,13 @@ $('.list-card-details').click( function() {
  *  Event: Card is refreshed
  *************************************************/
 $(document).ready(function() {
-    // If autohide checked
     if( $('.js-checklist-list').length ) {
         setTimeout(function() {
-            // Autohide items
-            toggleCheckedItems('hide');
-            addCheckedToggle('show');
+            // If autohide checked
+            autohideCheckedItems();
+
+            // If add check/uncheck all button
+            addCheckUncheckButton();
         }, 1000);
     }
 });
@@ -66,6 +68,18 @@ function toggleCheckedItems(action) {
 // Add a button for toggling checked items
 function addCheckedToggle(action) {
     $('.window-module.other-actions .u-clearfix').append('<a class="button-link js-tt-toggle-checked" title="Toggle the finished checklist items." data-action="'+action+'"><span class="icon-sm icon-subscribe"></span> '+toTitleCase(action)+' Checked</a>');
+}
+
+// Autohide all the checked list items
+function autohideCheckedItems() {
+    toggleCheckedItems('hide');
+    addCheckedToggle('show');
+}
+
+// Add check/uncheck all button
+function addCheckUncheckButton() {
+    $('.window-module.checklist-list .window-module-title-options').prepend('<a class="hide-on-edit quiet js-check-all-items" href="#">Check All</a>');
+    console.log('add check/uncheck all button');
 }
 
 // Convert a string to title case
